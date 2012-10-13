1c7d4b5b6f109b29033d819f2daebb4124245980
siyaomin <yaominsi10@gmail.com>
2012-10-14 00:50:30 +0800

    init

---
+++ src/main/java/lou19/com/votem/App.java
@@ -0,0 +1,13 @@
+package lou19.com.votem;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
